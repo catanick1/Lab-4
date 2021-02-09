ec5bbf2a86a149ceb961dc60aa1e1cc70abddb62
Calvin Catania <catanick@mail.uc.edu>
2021-02-09 16:46:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,44 @@
+/*
+Lab 4
+Partners: Calvin Catania and Nicholas Natale
+Date: 2/9/2021
+*/
+
+import java.util.Random;
+class Main 
+  {
+  public static void main(String[] args) 
+  {
+    //assign random variable
+    Random r = new Random();
+    int randomNumber = r.nextInt(50) + 1;
+
+    System.out.println("The Random number is: " + randomNumber);
+
+    countDown(randomNumber);
+
+    //specify countdown messages
+    if (randomNumber <= 5) 
+    {
+      System.out.println("Ahoy mateys");
+    } 
+    else if (randomNumber > 25 && randomNumber < 42) 
+    {
+      System.out.println("Cannonball!");
+    } 
+    else 
+    {
+      System.out.println("Blast off!");
+    }
+  }
+  
+  //print next number and stop at zero
+  static void countDown(int theRandNum) 
+  {
+      while(theRandNum != 0) 
+      {
+        theRandNum--;
+        System.out.println(theRandNum);
+      }
+  }
+}
